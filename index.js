1f15eca33adbb8f812601bcde365b011316947cf
tatsu <naz1mb20@gmail.com>
2023-02-03 11:33:42 +0000

    fontawesome blank page

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ mongoose.set('strictQuery', true);
 require('dotenv').config();
 
 const app = express();
-//const bodyParser = require('body-parser');
 // instead of body-parser
-//app.use(express.urlencoded({extended: true})); 
-app.use(express.urlencoded()); 
+app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
 app.set("view engine", "ejs");
@@ -17,5 +15,16 @@ app.route("/")
   .get((req,res) => {
     res.render("pages/index");
   })
+
+try{
+  mongoose.connect('mongodb://127.0.0.1:27017/iftariSponsorship');
+} catch (error) {
+  console.log(error);
+}
+const sponsorSchema = new mongoose.Schema({
+  rDate : Number,
+  date : Date,
+  
+});
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"));
