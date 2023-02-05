c74d1d9ad6c050048f8ff91c516078a121925f36
tatsu <naz1mb20@gmail.com>
2023-02-05 20:47:28 +0000

    index page func complete

--- index.js
+++ index.js
@@ -11,11 +11,37 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.route("/")
-  .get((req,res) => {
-    res.render("pages/index");
-  })
+// create date rage
+Date.prototype.addDays = function(days) {
+  var dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days);
+  return dat;
+}
 
+function getDates(startDate, stopDate) {
+ var dateArray = new Array();
+ var currentDate = startDate;
+ while (currentDate <= stopDate) {
+   dateArray.push(currentDate)
+   currentDate = currentDate.addDays(1);
+ }
+ return dateArray;
+}
+
+const dateArray = getDates(new Date(2023, 2, 23), (new Date(2023, 2, 23)).addDays(29));
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric"
+};
+console.log(dateArray.length);
+let dateDisplay = [];
+for (i = 0; i < dateArray.length; i ++ ) {
+  dateDisplay.push(dateArray[i].toLocaleString("en-GB", options));
+  // console.log(dateArray[i].toLocaleString("en-GB", options));
+}
 try{
   mongoose.connect('mongodb://127.0.0.1:27017/iftariSponsorship');
 } catch (error) {
@@ -24,7 +50,44 @@ try{
 const sponsorSchema = new mongoose.Schema({
   rDate : Number,
   date : Date,
-  
+  name: String,
+  cateringBySponsor: Boolean,
+  paid: Boolean,
+  caterer: String,
+  cookingCost: mongoose.Decimal128,
+  ingCost: mongoose.Decimal128,
+  confirmed: Boolean
 });
+
+const Sponsor = mongoose.model("Sponsor", sponsorSchema);
+
+app.route("/")
+  .get(async (req,res) => {
+    try{
+      const data = await Sponsor.find({});
+      console.log(data);
+      res.render("pages/index", {content: data, dates: dateDisplay});
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+app.route("/day/:rDate")
+  .get(async (req,res) => {
+    try{
+      const rDate = req.params.rDate;
+      const dayDetails = await Sponsor.findOne({rDate: rDate});
+      console.log(dayDetails);
+      res.render("pages/sponsorForm", {content: dayDetails});
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+app.route("/sponsor")
+  .post(async (req,res) => {
+    console.log("posted");
+    res.redirect("/")
+  });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"));
